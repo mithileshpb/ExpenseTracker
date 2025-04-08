2b4932840a789a175db131e45dfbdbdada6feaca
mithileshpb <mithileshg250@gmail.com>
2025-04-08 09:20:50 +0530

    update txt file ,setting and add one model

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%%!upfkl9%#4ex)(uiop*^v&va7zw-d(7@3m@y7zc&k^t676)9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
